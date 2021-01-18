67963826b878f96d8a15fbcc9ba79362d87cd27d
machadoluiz <lhrmachado@outlook.com>
2021-01-18 01:40:23 +0000

    commit

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -24,6 +24,9 @@ class Layout extends Component {
           <html lang="pt-BR" />
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="black-translucent"></meta>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
