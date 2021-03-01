05644f273d26b2b9136f86eb0750124026f9f3cd
machadoluiz <lhrmachado@outlook.com>
2021-03-01 12:26:14 +0000

    commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -90,6 +90,12 @@ module.exports = {
       },
     },
     {
+      resolve: "gatsby-plugin-react-leaflet",
+      options: {
+        linkStyles: true,
+      },
+    },
+    {
       resolve: "gatsby-plugin-netlify-cms",
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`,
