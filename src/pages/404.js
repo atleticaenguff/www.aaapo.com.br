0845b920c9ba14419729ed1fe1b892efbefaea4d
Luiz Machado <lhrmachado@outlook.com>
2021-06-01 00:06:37 +0100

    Fix 404 page and meta image

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,6 @@
 import React from "react"
+import Helmet from "react-helmet"
+import "../components/Layout.scss"
 import Seo from "../components/Seo"
 import { Link } from "gatsby"
 
@@ -9,6 +11,16 @@ const NotFoundPage = () => {
 
   return (
     <>
+      <Helmet defer={false}>
+        <html lang="pt-BR" />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </Helmet>
       <Seo metaTitleSeo={metaTitle} metaDescriptionSeo={metaDescription}></Seo>
       <section className="hero is-fullheight-with-navbar">
         <div className="hero-body">
